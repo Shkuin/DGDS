230601d9cab39b094fb0f52cd3f135c981d433b5
Artem Melentev <tema2208st@mail.ru>
2023-06-05 01:28:23 +0700

    Added interaction with developer and buy game contract

--- base/scripts_for_contract/connect_to_contract.py
+++ base/scripts_for_contract/connect_to_contract.py
@@ -2,17 +2,21 @@ from web3 import Web3
 import json
 from web3.middleware import geth_poa_middleware
 from eth_account import Account
+from .developer_contract.interaction_with_developer_contract import (
+    DeveloperContract,
+)
+from .buy_game_contract.interaction_with_buy_game_contract import (
+    BuyGameContract,
+)
 
 
-def read_json(name):
-    with open(
-        f"base/scripts_for_contract/developer_contract_info/{name}.json", "r"
-    ) as file:
+def read_json(path):
+    with open(f"base/scripts_for_contract/{path}.json", "r") as file:
         info = json.load(file)
     return info
 
 
-def get_developer_contract(w3, contract_address, abi):
+def get_deployed_contract(w3, contract_address, abi):
     contract = w3.eth.contract(address=contract_address, abi=abi)
     return contract
 
@@ -30,37 +34,27 @@ def create_web3_account(w3):
     return account
 
 
-def create_developer_nft(metadata_uri, owner, account, w3, contract):
-    transaction = contract.functions.createDeveloperNFT(
-        metadata_uri, owner
-    ).buildTransaction(
-        {
-            "from": account.address,
-            "gas": 200000,
-            "gasPrice": w3.toWei("40", "gwei"),
-            "nonce": w3.eth.getTransactionCount(account.address),
-        }
-    )
-    signed_txn = w3.eth.account.sign_transaction(
-        transaction, private_key=account.privateKey
-    )
-    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-    token_id = contract.functions.tokenId().call()
-    return tx_hash, token_id
-
-
-def main(metadata_uri, owner_address):
-    info = read_json("developer_contract_info")
+def convert_info_json(info, name):
     contract_address = info["contract_address"]
     w3 = Web3(Web3.HTTPProvider(info["http_provider"]))
-    abi = read_json(contract_address)["abi"]
+    abi = read_json(f"{name}_contract/info/{contract_address}")["abi"]
 
-    contract = get_developer_contract(w3, contract_address, abi)
+    contract = get_deployed_contract(w3, contract_address, abi)
     account = create_web3_account(w3)
+    return contract, account, w3
 
-    tx_hash, token_id = create_developer_nft(
-        metadata_uri, owner_address, account, w3, contract
-    )
 
-    return token_id
+def get_web3_info(name):
+    info = read_json(f"{name}_contract/info/{name}_contract_info")
+    return convert_info_json(info, name)
+
+
+def return_contract(contract_type):
+    if contract_type == "Developer":
+        name = "developer"
+        contract, account, w3 = get_web3_info(name)
+        return DeveloperContract(account, contract, w3)
+    if contract_type == "BuyGame":
+        name = "buy_game"
+        contract, account, w3 = get_web3_info(name)
+        return BuyGameContract(account, contract, w3)
--- base/views.py
+++ base/views.py
@@ -6,9 +6,23 @@ from base.scripts_for_ipfs import create_metadata
 from base.scripts_for_contract import connect_to_contract
 
 
-def add_new_game(name, genre, description, platform, poster, images, price, token_id, private_key):
-    new_game = Game.objects.create(name=name, genre=genre, description=description, platform=platform, poster=poster, images=images, price=price, token_id=token_id, private_key=private_key, slug=str(name).lower().replace(" ", "_"))
+def add_new_game(
+    name, genre, description, platform, poster, images, price, token_id, private_key
+):
+    new_game = Game.objects.create(
+        name=name,
+        genre=genre,
+        description=description,
+        platform=platform,
+        poster=poster,
+        images=images,
+        price=price,
+        token_id=token_id,
+        private_key=private_key,
+        slug=str(name).lower().replace(" ", "_"),
+    )
     new_game.save()
+
 
 def main_page(request):
     return render(request, "main_page.html")
@@ -35,9 +49,39 @@ def game_uploading(request):
             ) = create_metadata.convert_game_file_to_metadata(game_file)
             # u should write real address here, owerwise there will be an error
             wallet_address = "0x99bc949975C4bd87D2a6d2a5043112C121EC68D1"
-            token_id = connect_to_contract.main(game_file_uri, wallet_address)
-            poster_uri, images_uri = create_metadata.create_metadata_json(poster, images)
-            add_new_game(name, genre, description, platform, poster_uri, images_uri, price, token_id, key)
+
+            developer_contract = connect_to_contract.return_contract("Developer")
+
+            # sometimes u have to wait, because it takes time to load into blockchain, that's why sometimes token_id will be wrong
+            token_id = developer_contract.create_developer_nft(
+                encrypted_message, wallet_address
+            )
+
+            # example how use buy_game contract
+            buy_game_contract = connect_to_contract.return_contract("BuyGame")
+            eth_amount = 10000000000000000  # = 0.01 eth
+            info = buy_game_contract.get_metadata_from_contract(
+                eth_amount,
+                "0xc1275DF70ef313C86C5Ac60De81Af1AA9f1F71De",
+                1,
+                "0x99bc949975C4bd87D2a6d2a5043112C121EC68D1",
+            )
+            print("Retrieved information:", info)
+
+            poster_uri, images_uri = create_metadata.create_metadata_json(
+                poster, images
+            )
+            add_new_game(
+                name,
+                genre,
+                description,
+                platform,
+                poster_uri,
+                images_uri,
+                price,
+                token_id,
+                key,
+            )
 
             return render(request, "main_page.html")
     else:
@@ -47,14 +91,13 @@ def game_uploading(request):
 
 
 def game_catalog(request):
-    context = {
-        'games' : Game.objects.all()
-    }
+    context = {"games": Game.objects.all()}
     return render(request, "catalog/game_catalog.html", context)
+
 
 def game_detail(request, slug):
     game = Game.objects.get(slug__iexact=slug)
-    if request.method=='POST':
+    if request.method == "POST":
         return HttpResponseRedirect(game.get_absolute_url())
 
-    return render(request, 'catalog/game_detail.html', context={'game': game})
+    return render(request, "catalog/game_detail.html", context={"game": game})
