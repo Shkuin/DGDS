b53aaea8e03925e626a5f601c355ec6098395f23
Artem Melentev <tema2208st@mail.ru>
2023-06-22 20:45:38 +0700

    Fixed bugs in base.html
    Fixed buyer address storage mechanism

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("upload/", views.game_uploading, name="upload"),
     path("catalog/", views.game_catalog, name="catalog"),
     path("catalog/<str:slug>/", views.game_detail, name="game_detail_url"),
+    # path("js_script/<str:slug>/", views.game_detail, name="game_detail_url"),
 ]
